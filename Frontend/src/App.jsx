3fd6d512ea1253437697d7f5fdefdd090b46c036
dp <codewithdp2073@gmail.com>
2025-01-04 22:35:49 +0530

    Frontend setup using vite complated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-9xl bg-red-500 outline-double p-5  text-center m-5 mb-5'>App</div>
+  )
+}
+
+export default App
